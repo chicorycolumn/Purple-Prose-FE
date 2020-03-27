0f1c0b088fc88dfb6a49a282b03067fd462ba840
C <chrismatusemail@gmail.com>
2020-03-27 13:32:33 +0000

    It's live and all works! Also, have just added rudimentary Users page.

---
+++ build/precache-manifest.efa51208f169d4c4ef0e2d749f6b01f1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "401fd5f26f26aafb6a0f9a3f15c27130",
+    "url": "/index.html"
+  },
+  {
+    "revision": "432a8a809b8a5cedf517",
+    "url": "/static/css/main.2309ead7.chunk.css"
+  },
+  {
+    "revision": "d60502f6f6be6db4cae1",
+    "url": "/static/js/2.cce2c0a4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.cce2c0a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "432a8a809b8a5cedf517",
+    "url": "/static/js/main.5a2c21e4.chunk.js"
+  },
+  {
+    "revision": "a5724e96f8a9dc8a1715",
+    "url": "/static/js/runtime-main.52b6ed6a.js"
+  },
+  {
+    "revision": "f0b3d0dec42fe7f9910485030288cb38",
+    "url": "/static/media/400-p.f0b3d0de.jpg"
+  },
+  {
+    "revision": "65bc16823286c3c2ee6e4aa88146fda1",
+    "url": "/static/media/404-p.65bc1682.jpg"
+  },
+  {
+    "revision": "f5b09aa482c9cba8d489e814efb9d34c",
+    "url": "/static/media/500-p.f5b09aa4.jpg"
+  },
+  {
+    "revision": "46de4a75498c4deb366c76415e4e2ed6",
+    "url": "/static/media/emptypage.46de4a75.jpg"
+  },
+  {
+    "revision": "5e434e4e66a73032b909fdd706a94801",
+    "url": "/static/media/naughty.5e434e4e.jpg"
+  },
+  {
+    "revision": "3bbf4c2d387686781e06e3ac187c4a58",
+    "url": "/static/media/rush.3bbf4c2d.png"
+  }
+]);
